1ee3cf46d56ddec6a994131e8faef510fc53be4e
Shekhar.Thakur04 <93523035+Shekhar505@users.noreply.github.com>
2022-07-31 20:09:17 +0530

    Added a view engine

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 3200;
 
 app.use('/', require('./routes/index'))
 
+//Setting up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log("Error in starting up the Server at " + port);
